3a7b78fe8759e548aae7c99210116d49586ac9b3
KoolShow <51787949+KoolShow@users.noreply.github.com>
2021-11-06 12:07:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,16 @@ function cartesian(arr) {
 		return res;
 	});
 };
-
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1] || true;}
+       }
+       return(false);
+};
 p = cartesian([lhhd, lhhd]);
 function Jia(str, level){
 	sz = [];
